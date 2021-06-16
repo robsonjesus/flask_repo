f234c422b23f5abcff8e00aee5cd3c29b6cd6f6f
Robson Jesus <rob.nogueira@terra.com.br>
2021-06-16 14:55:55 -0300

    vazio

--- doc1.py
+++ doc1.py
@@ -12,17 +12,10 @@ import threading
 app = Flask(__name__)
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/")
 def home():
-    if request.method == "POST":
-        todo = request.form.get("api_key")
-        api_password = request.form.get("api_secret")
-        moeda = request.form.get("moeda")
-        print(todo)
-        print(api_password)
-        print(moeda)
-        return render_template('principal.html')
-    return render_template('index.html')
+ 
+    return 'Server Rodando'
 
 
 const PORT = process.env.PORT || '8080'
